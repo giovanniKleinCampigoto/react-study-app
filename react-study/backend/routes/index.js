0ac6eadb80046bba62fe179cac08571dda31495e
gcampigo <giovannikleincampigoto@gmail.com>
2018-03-19 10:18:39 -0300

    Starting to build endpoints...

--- react-study/backend/routes/index.js
+++ react-study/backend/routes/index.js
@@ -3,5 +3,4 @@ const survivorRoutes = require('./survivor_routes');
 
 module.exports = function(app, db) {
   survivorRoutes(app, db);
-  // Other route groups could go here, in the future
 };
--- react-study/backend/routes/survivor_routes.js
+++ react-study/backend/routes/survivor_routes.js
@@ -2,7 +2,6 @@
 
 module.exports = function(app, db) {
     app.post('/survivors', (req, res) => {
-        // You'll create your note here.
         res.send('Hello')
       });
 };
